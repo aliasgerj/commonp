77be3930e664452f9bbb554de70170ebd34d2cf9
Aliasger Jaffer <aliasgerjaffer@ali.home>
2024-04-04 13:55:37 -0400

    Using info_token with lib printerp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core';
+import {Component, Inject} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { PrinterpComponent } from 'printerp'
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PrinterpComponent ],
   template: `
     <h1>Welcome to {{title}}!</h1>
-
+<lib-printerp></lib-printerp>
     <router-outlet />
   `,
   styles: [],
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,14 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { INFO_TOKEN } from 'commonp';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), {
+    provide: INFO_TOKEN,
+    useValue: {
+      name: 'Angular',
+      country: 'USA'
+    }
+  }]
 };
